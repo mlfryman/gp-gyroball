81aac97f2efd3cb0c4ddf2dd8ce9dd4fad81a84c
Melanie L. Fryman <melanie@frymanet.com>
2014-10-16 11:53:57 -0500

    working on gyroscope

--- www/js/MainCtrl.js
+++ www/js/MainCtrl.js
@@ -16,6 +16,29 @@
     $scope.$on('timer-stopped', function(event, data){
       console.log('Timer Stopped - data = ', data);
     // END Timer
+
+    function success(orientation){
+      $scope.alpha = orientation.alpha;
+      $scope.beta = orientation.beta;
+      $scope.gamma = orientation.gamma;
+      $scope.absolute = orientation.absolute;
+      console.log(orientation);
+    }
+
+    console.log(success, error);
+
+    function error(o){
+      console.log('ERROR:', o);
+    }
+
+    $scope.start = function(){
+      navigator.gyroscope.watchGyroscope(success, error);
+      console.log(success, error);
+    };
+
+    $scope.reset = function(){
+      navigator.gyroscope.clearWatch();
+    };
     });
   }]);
 })();
