976684018e66a9dd943ec70b0d852390a8cf6eba
Melanie L. Fryman <melanie@frymanet.com>
2014-10-16 14:18:24 -0500

    saving point

--- www/js/MainCtrl.js
+++ www/js/MainCtrl.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('gyroball', ['ionic', 'timer'])
-  .controller('MainCtrl', ['$scope', function($scope){
+  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
     // TIMER
-    $scope.timerRunning = true;
+    $scope.timerRunning = false;
     $scope.startTimer = function(){
       $scope.$broadcast('timer-start');
       $scope.timerRunning = true;
@@ -20,28 +20,33 @@
     // GYROSCOPE
     function success(orientation){
       console.log('------SUCCESS!-------\n' +
-        'orientation= ' + '\n' +
+        'ORIENTATION= ' + '\n' +
         'alpha: ' + orientation.alpha + '\n' +
         'beta: ' + orientation.beta + '\n' +
         'gamma: ' + orientation.gamma + '\n' +
         'absolute: ' + orientation.absolute + '\n');
+      $scope.orientation = orientation;
     }
 
-    function error(){
-      alert('ERROR!');
+    function error(err){
+      alert('ERROR!', err);
     }
 
     $scope.start = function(){
       console.log('-------Starting Gyroscope-------');
-      navigator.gyroscope.watchGyroscope(success, error);
+      navigator.gyroscope.watchGyroscope(success, error, {frequency:500}).then(function(){
+        $scope.startTimer();
+      });
     };
 
-    $scope.reset = function(){
+    $scope.reset = function(id){
       // @param {String} id - the id of the watch returned from #watchGyroscope.
-      navigator.gyroscope.clearWatch();
+      console.log('-------Reset Gyroscope-------');
+      navigator.gyroscope.clearWatch(id);
+      $scope.stopTimer();
     };
     // END Gyroscope
     // place ball in random location
-    });
+  });
   }]);
 })();
