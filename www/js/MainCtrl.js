9a7111b1ba55d19d365885047fd7a2e5c4c5aac4
Melanie L. Fryman <melanie@frymanet.com>
2014-10-16 11:02:08 -0500

    added timer

--- www/js/MainCtrl.js
+++ www/js/MainCtrl.js
@@ -1,7 +1,21 @@
 (function(){
   'use strict';
 
-  angular.module('gyroball', ['ionic'])
+  angular.module('gyroball', ['ionic', 'timer'])
   .controller('MainCtrl', ['$scope', function($scope){
+    // Timer
+    $scope.timerRunning = true;
+    $scope.startTimer = function(){
+      $scope.$broadcast('timer-start');
+      $scope.timerRunning = true;
+    };
+    $scope.stopTimer = function(){
+      $scope.$broadcast('timer-stop');
+      $scope.timerRunning = false;
+    };
+    $scope.$on('timer-stopped', function(event, data){
+      console.log('Timer Stopped - data = ', data);
+    // END Timer
+    });
   }]);
 })();
