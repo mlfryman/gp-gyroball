953ecdb4b2e108d5376b9e554ca4b6a49ba167c2
Joy Pratt <joympratt@gmail.com>
2014-10-17 11:37:24 -0500

    adds ball factory

--- www/js/ball.js
+++ www/js/ball.js
@@ -8,9 +8,11 @@
   //  yPos: Game.playground.height - 30
   //  });
 
-  var Ball = {
+  angular.module('gyroball')
+  .factory('Ball', [function(){
+    var Ball = {
 
-    init: function(settings) {
+      init: function(settings) {
         this.size = settings.size;
         this.status = 'rolling'; // rolling, crashing, onabrick
         this.position = {
@@ -18,85 +20,87 @@
             y: settings.yPos
         };
 
-        // Draw the ball
+          // Draw the ball
         this.draw();
     },
 
-    // Draw the ball
+      // Draw the ball
     draw: function() {
-        Game.playgroundContext.fillStyle = '#8d2544';
-        Game.playgroundContext.beginPath();
-        Game.playgroundContext.arc(this.position.x, this.position.y, this.size / 2, 0, 2 * Math.PI);
-        Game.playgroundContext.closePath();
-        Game.playgroundContext.fill();
+      Game.playgroundContext.fillStyle = '#8d2544';
+      Game.playgroundContext.beginPath();
+      Game.playgroundContext.arc(this.position.x, this.position.y, this.size / 2, 0, 2 * Math.PI);
+      Game.playgroundContext.closePath();
+      Game.playgroundContext.fill();
     },
 
     // Ball movement
     roll: function(motionX, motionY) {
 
-        this.position.y += motionY;
-        this.position.x += motionX;
+      this.position.y += motionY;
+      this.position.x += motionX;
 
-        Game.clearPlayground();
-        Target.draw();
-        this.draw();
-        Boundaries.draw();
-        Obstacles.draw();
+      Game.clearPlayground();
+      Target.draw();
+      this.draw();
+      Boundaries.draw();
+      Obstacles.draw();
     },
 
     /*
      * Make the ball crash against boundaries
      */
     crash: function(outofboundaries) {
-        this.status = 'crashing';
+      this.status = 'crashing';
 
-        if (outofboundaries === 'left') {
-            this.position.x = Boundaries.left - 0 + (this.originalSize / 2);
-        }
-        else if (outofboundaries === 'top') {
-            this.position.y = Boundaries.top - 0 + (this.originalSize / 2);
-        }
-        else if (outofboundaries === 'right') {
-            this.position.x = Boundaries.top - 0 + Boundaries.width - (this.originalSize / 2);
-        }
-        else if (outofboundaries === 'bottom') {
-            this.position.y = Boundaries.left - 0 + Boundaries.height - (this.originalSize / 2);
-        }
+      if (outofboundaries === 'left') {
+        this.position.x = Boundaries.left - 0 + (this.originalSize / 2);
+      }
+      else if (outofboundaries === 'top') {
+        this.position.y = Boundaries.top - 0 + (this.originalSize / 2);
+      }
+      else if (outofboundaries === 'right') {
+        this.position.x = Boundaries.top - 0 + Boundaries.width - (this.originalSize / 2);
+      }
+      else if (outofboundaries === 'bottom') {
+        this.position.y = Boundaries.left - 0 + Boundaries.height - (this.originalSize / 2);
+      }
 
-        this.draw();
+      this.draw();
     },
+
     /*
      * fall
      * Make the ball fall into the hole
      */
     fall: function(x, y) {
-        /* Update ball status */
-        this.status = 'falling';
+      /* Update ball status */
+      this.status = 'falling';
 
-        /* Update ball position */
-        this.position.x = x;
-        this.position.y = y;
+      /* Update ball position */
+      this.position.x = x;
+      this.position.y = y;
 
-        /* Decrease ball size */
-        this.size -= 1;
+      /* Decrease ball size */
+      this.size -= 1;
 
-        /* Repaint */
-        Game.clearPlayground();
-        Target.draw();
-//        Boundaries.draw();
-//        Obstacles.draw();
-        this.draw();
+      /* Repaint */
+      Game.clearPlayground();
+      Target.draw();
+      //  Boundaries.draw();
+      //  Obstacles.draw();
+      this.draw();
 
-        /* Animate until the ball is visible */
-        if (this.size > 0) {
-            var self = this;
-            window.requestAnimationFrame(function() {
-                self.fall(x, y);
-            });
-        }
-        else {
-            this.status = 'rolling';
-        }
+      /* Animate until the ball is visible */
+      if (this.size > 0) {
+        var self = this;
+        window.requestAnimationFrame(function() {
+          self.fall(x, y);
+        });
+      }
+      else {
+        this.status = 'rolling';
+      }
     }
-};
+  };
+  }]);
 })();
